b2dcf880a4b4672d9888faf82614b3a3a933d7a2
ElErizio <theericyea100@hotmail.com>
2022-04-08 21:53:04 -0500

    Se agrega el proyecto

---
+++ Proyecto Eric Examen 2/Proyecto Eric Examen 2.cpp
@@ -0,0 +1,14 @@
+// Proyecto Eric Examen 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+  cout << "Hello World!" << endl;
+
+
+}
